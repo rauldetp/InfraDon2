23c4002ecabc05ac2d7b16bde5a0aa6918833342
polymnie-io <laurent@polymnie.io>
2025-10-02 14:51:38 +0200

    Fix tsBuildInfoFile eslint error

--- InfraDon2nosqlraulg/src/components/TheWelcome.vue
+++ InfraDon2nosqlraulg/src/components/TheWelcome.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import {ref} from 'vue'
+import { ref } from 'vue'
 
-const counter = ref(110);
+const counter = ref(110)
 
 const increment = () => {
   counter.value++
@@ -9,7 +9,7 @@ const increment = () => {
 </script>
 
 <template>
-<h1>SALUT LE JIIIIIIISTRERIIIIEWWWW</h1>
-<p> Counter {{ counter }}</p>
-<button @click="increment">Increment</button>
+  <h1>SALUT LE JIIIIIIISTRERIIIIEWWWW</h1>
+  <p>Counter {{ counter }}</p>
+  <button @click="increment">Increment</button>
 </template>
