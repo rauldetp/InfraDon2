dfb840a09f5b007419f34f432d7310115789bbda
Raul Gomez <raul.gomezcalvo@heig-vd.ch>
2025-10-09 11:51:53 +0200

    V2

--- InfraDon2nosqlraulg/src/components/TheWelcome.vue
+++ InfraDon2nosqlraulg/src/components/TheWelcome.vue
@@ -1,11 +1,47 @@
-<script setup lang="ts">
-import { ref } from 'vue'
+<script lang="ts">
+import { defineComponent } from 'vue'
+import PouchDB from 'pouchdb'
 
-const counter = ref(110)
+export default defineComponent({
+  name: 'TestDatabase',
 
-const increment = () => {
-  counter.value++
-}
+  data() {
+    return {
+      counter: 110
+    }
+  },
+
+  methods: {
+    increment() {
+      this.counter++
+    },
+
+    initDatabase() {
+      // Connexion à CouchDB (adapter l’URL selon ta base)
+      const db = new PouchDB('http://admin:admin@localhost:5984/infra_53_0850')
+
+      if (db) {
+        console.log('Connexion réussie à CouchDB')
+      } else {
+        console.error('Erreur : objet DB nul')
+      }
+
+      // Vérifie les informations de la base
+      db.info()
+        .then((info: PouchDB.Core.DatabaseInfo) => {
+          console.log('Informations de la base :', info)
+        })
+        .catch((err: Error) => {
+          console.error('Erreur lors de la lecture :', err.message)
+        })
+    }
+  },
+
+  mounted() {
+    // Appel automatique quand le composant est monté
+    this.initDatabase()
+  }
+})
 </script>
 
 <template>
